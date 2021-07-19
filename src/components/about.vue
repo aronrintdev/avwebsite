6bd009ed94905b78ee0ae520cf53cfec29557208
aronrint <aronrint@gmail.com>
2021-07-19 09:04:43 -0700

    Update about page

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,36 +1,23 @@
 <template>
   <div id="about" class="no-pad">
-    <header class="hero hero__founder" style="background-image: url(/public/img/av-1.jpg);">
+    <header class="hero hero__team" style="background-image: none;">
       <h1 class="hero-header">
-        <span class="founder-header--name">About</span>
+        <img data-v-6a232478="" src="/public/img/logos/av.png">
       </h1>
     </header>
     <div class="wrapper inner-wrapper">
-      <div class="message clearfix message--only">
-        <div class="sideways-heading">
-            <span class="sideways-heading--text">
-                Mission
-            </span>
-        </div>
-        <div class="message-text">
-            <p>
-              Authentic Ventures (AV) is a seed and early stage Venture Capital Firm that believes that a strong, inclusive network of founders, operators and investors can accomplish great things. We have a particular interest in women and minority founders that are solving hard problems. We use our diverse network of talent to help our companies and each other succeed.
-            </p>
-            <p>
-              Find out more about who we invest in on our <router-link :to="{ name: 'founders' }">Founders’ Page</router-link>.
-            </p>
-        </div>
-      </div>
-
       <div class="about-bio grid-container clearfix">
         <div class="grid-x grid-margin-x">
           <div class="about-bio--image medium-5 cell">
-            <img src="public/img/founders/lindsay.jpg" />
+            <img src="public/img/founders/lindsay-sq.jpg" />
           </div>
           <div class="about-bio--text medium-7 cell">
-            <h3>Lindsay lee – Managing Member</h3>
+            <h3>LINDSAY LEE – MANAGING MEMBER</h3>
             <p>
-                Lindsay has been an investor in technology for almost 16 years and has developed a strong network in Silicon Valley over the last 20+ years. Lindsay started his career at Merrill Lynch. Immediately following graduate school at Stanford, he co-founded a financial services software company.  He subsequently served as CIO for an investment management firm he helped to grow to almost $1B. He then started a small direct investment fund that invested primarily in public technology companies. Most recently, he managed a family office where he built and managed a very successful seed and early stage venture portfolio. Lindsay received his BA in Economics (Phi Beta Kappa) from the University of Florida where he was awarded Outstanding Male Graduate. He received his JD and MBA degrees from Stanford University.
+                Over his nearly 20 years as an investor in Silicon Valley, Lindsay has served as CIO for an investment management firm he helped grow to almost $1B and managed a family office where he built and managed an extremely successful seed and early-stage venture portfolio. He also co-founded a financial services software company and ran a direct investment fund.
+            </p>
+            <p>
+              Lindsay holds an MBA from Stanford’s Graduate School of Business and a J.D. from Stanford Law School. He also graduated Phi Beta Kapa in Economics from the University of Florida.
             </p>
             <p><router-link :to="{ name: 'investments'}">Prior Investments</router-link></p>
           </div>
@@ -45,7 +32,27 @@
           <div class="about-bio--text medium-7 cell">
             <h3>Lisa Riedmiller – Chief Financial Officer</h3>
             <p>
-                Lisa has over 27 years of venture capital fund accounting, investor relations and fund administration experience. Lisa successfully built her specialized venture capital services business to target the niche market of smaller and developing funds by offering her clients high-level financial support without the burden of full-time overhead. Some of Lisa’s current clients include: Amplify.LA, BioStar Ventures, Blade Ventures, Crosscut Ventures, Lagunita Biosciences LLC, Millennium Life Sciences Fund, MuckerLabs, Palomar Ventures, and Windward Ventures.
+                Lisa has over 27 years of venture capital fund accounting, investor relations and fund administration experience. Lisa specializes in developing and emerging venture capital funds.
+            </p>
+            <p>
+              Prior clients have included: Amplify.LA, BioStar Ventures, Blade Ventures, Crosscut Ventures, Lagunita Biosciences LLC, Millennium Life Sciences Fund, MuckerLabs, Palomar Ventures, and Windward Ventures.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="about-bio grid-container clearfix">
+        <div class="grid-x grid-margin-x">
+          <div class="about-bio--image medium-5 cell">
+            <img src="public/img/founders/michael.jpg" />
+          </div>
+          <div class="about-bio--text medium-7 cell">
+            <h3>MICHAEL NICHOLS, DIRECTOR OF OPERATIONS AND CHIEF COMPLIANCE OFFICER</h3>
+            <p>
+                Michael has served startup and other clients as an attorney and operations lead for 20 years. He has worked at leading international law firms, including Morrison and Foerster, and has served as General Counsel and in-house counsel at startup companies funded by VC firms including Khosla Ventures and the Mayfield Fund.
+            </p>
+            <p>
+                Michael holds a J.D. from the Yale Law School and A.B. (Honors) and M.A. degrees from Stanford University.
             </p>
           </div>
         </div>
@@ -65,4 +72,8 @@
 
 <style lang="scss" scoped>
   @import '../css/settings';
+
+  .hero-header img {
+    max-width: 440px;
+  }
 </style>
