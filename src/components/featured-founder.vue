afbd17ca1a7812cd1bde3136a0b6dd826928aee8
aronrint <aronrint@gmail.com>
2021-07-19 02:40:40 -0700

    Update homepage

--- src/components/featured-founder.vue
+++ src/components/featured-founder.vue
@@ -8,14 +8,17 @@
             </router-link>
         </div>
         <div class="featured-founder--images--small">
-            <router-link :to="{ name: 'camille-hearst' }"><img src="public/img/kit-logo.png" /></router-link>
+            <a href="https://patreon.com"><img src="public/img/kit-patreon.png" /></a>
             <router-link :to="{ name: 'camille-hearst' }" class="view-link">View Profile</router-link>
         </div>
     </div>
     <div class="featured-founder--bio">
         <h3>Camille Hearst</h3>
         <p>
-            Camille believes that by leveraging all kinds of people, thoughts, ideas, opinions and backgrounds, we can help and learn from each other. She is CEO and co-founder of KIT, a company that helps creators share products they love with their followers.
+          Camille believes that by leveraging all kinds of people, thoughts, ideas, opinions and backgrounds, we can help and learn from each other. Camille co-founded KIT, a company that helps creators share products they love with their followers.
+        </p>
+        <p>
+          Kit was acquired in 2018 by Patreon, where Camille now serves as head of product. Patreon is the leading platform for creators to connect directly with their fans and followers
         </p>
     </div>
   </div>
--- src/components/home.vue
+++ src/components/home.vue
@@ -11,12 +11,15 @@
             <div class="message">
                 <div class="sideways-heading">
                     <span class="sideways-heading--text">
-                        Message
+                        We Believe
                     </span>
                 </div>
                 <div class="message-text">
                     <p>
-                        Great founders solving hard problems, might be the single greatest challenge of our generation. Wherever and whoever they are – we endeavor to find them. Solutions to hard problems are more likely to come from diverse teams. Our mission is to seek out unrecognized talent, especially with respect to women and people of color. To many, this talent is invisible. But they exist. And we believe that the next world changing idea will, in fact, come from one of them.
+                        Talented founders solving hard problems. Wherever and whoever they are – we endeavor to find them.
+                    </p>
+                    <p>
+                        Solutions to hard problems are more likely to come from diverse teams. Although we partner with exceptional founders regardless of race or gender, we actively seek out unrecognized talent, especially from teams with diverse backgrounds.
                     </p>
                 </div>
             </div>
@@ -29,16 +32,8 @@
                     <span class="dash">–</span> Darren Walker, CEO, Ford Foundation
                 </cite>
             </div>
+            <featured-company />
             <featured-founder />
-             <div class="quote">
-                <blockquote>
-                    “Growing up in a creative household, I learned to be a creative thinker.”
-                </blockquote>
-                <cite class="uc">
-                    <span class="dash">–</span> Camille Hearst, Co-founder and CEO, Kit
-                </cite>
-            </div>
-            <values />
         </div>
     </div>
 </template>
@@ -49,6 +44,7 @@
     components: {
       'founder-grid': () => import(/* webpackChunkName: "founder-common" */ './founder-grid.vue'),
       'featured-founder': () => import(/* webpackChunkName: "founder-common" */ './featured-founder.vue'),
+      'featured-company': () => import(/* webpackChunkName: "founder-common" */ './featured-company.vue'),
       values: () => import(/* webpackChunkName: "home" */ './values.vue')
     }
   }
